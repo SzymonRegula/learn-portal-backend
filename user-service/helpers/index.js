9739810b24b7d0845d0039052d7be88c384e788a
Szymon Reguła <113403572+SzymonRegula@users.noreply.github.com>
2024-03-21 17:49:30 +0100

    add trainer add

--- user-service/helpers/index.js
+++ user-service/helpers/index.js
@@ -1,5 +1,6 @@
 import response from "./response.js";
 import getUserItem from "./getUserItem.js";
 import getItemByUserId from "./getItemByUserId.js";
+import getTrainerItem from "./getTrainerItem.js";
 
-export { response, getUserItem, getItemByUserId };
+export { response, getUserItem, getItemByUserId, getTrainerItem };
